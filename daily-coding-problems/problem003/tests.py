4b6cb60e92faebbc292d54ce72f3c9267886f54e
Jonathan Levin <jon.levin25@gmail.com>
2019-11-12 01:00:07 +0200

    #003: Add failing test case (node separator in value)

--- daily-coding-problems/problem003/tests.py
+++ daily-coding-problems/problem003/tests.py
@@ -1,10 +1,20 @@
 import unittest
 from solution.solution import *
 
+
 class Tests(unittest.TestCase):
+
+
     def test1(self):
         node = Node('root', Node('left', Node('left.left')), Node('right'))
         self.assertEqual(deserialize(serialize(node)).left.left.val, 'left.left')
+
+    def test_semicolons_in_value(self):
+        orig_node = Node('r;oot', Node('left'), Node('right'))
+        node = deserialize(serialize(orig_node))
+        assert node.val == 'r;oot'
+        assert node.left.val == 'left'
+        assert node.left.left == None
 
 if __name__ == "__main__":
     unittest.main()
