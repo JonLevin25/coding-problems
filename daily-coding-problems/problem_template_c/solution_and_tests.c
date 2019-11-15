cca95556b6c1bdc4c10763b23be95bd05627433e
Jonathan Levin <jon.levin25@gmail.com>
2019-11-15 18:04:57 +0200

    Add C template + rename python dir

---
+++ daily-coding-problems/problem_template_c/solution_and_tests.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <assert.h>
+
+// TODO: extract tests to separate file + automate compilation to task in vscode
+// TODO: learn more about c module linking/including for this
+int test(int n){
+    
+}
