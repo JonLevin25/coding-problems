37eceed4d001553a064104db95677e698a805ee3
Jonathan Levin <jon.levin@inceptionxr.com>
2019-11-21 19:04:09 +0200

    fix LargestList

--- daily-coding-problems/problem009_python/solution/solution.py
+++ daily-coding-problems/problem009_python/solution/solution.py
@@ -1,24 +1,29 @@
 from typing import List, Tuple, Callable
 
+class ListItem:
+    def __init__(self, idx: int, value: int):
+        self.idx = idx
+        self.value = value
+
+    def __repr__(self):
+        return f"(idx: {self.idx}, value: {self.value})"
+
+    def __gt__(self, item2):
+        return self.value > item2.value
+
+
 class LargestList:
     #TODO: make generic?
-    def __init__(self, size: int, largerThanFunc):
-        self.list: List[Tuple[int, int]] = []
+    def __init__(self, size: int):
+        self.list: List[ListItem] = []
         self.size: int = size
-        self.largerThanFunc: Callable[Tuple[int, int], bool] = largerThanFunc
 
-    def tryAdd(self, item):
-        if len(self.list) == 0:
-            self.list.append(item)
-            return
+    def tryAdd(self, item: ListItem):
         for i in range(0, self.size):
-            # if list empty at this point: add item here
-            # TODO: if list should support None as an item in the future, add support
-            if self.list[i] == None:
-                self.list[i] = item
+            if len(self.list) < i + 1:
+                self.list.append(item)
                 return
-
-            elif self.largerThanFunc(item, self.list[i]):
+            elif item > self.list[i]:
                 self.list.insert(i, item)
                 self._remove_excess()
                 return
@@ -30,35 +35,22 @@ class LargestList:
         for i in range(delta):
             self.list.pop()
 
+    def __getitem__(self, i: int):
+        return self.list[i]
+
 # TODO: NOT PASSING. Added testcase to demonstrate
 def max_non_neighbor_sum(arr: List[int]) -> int:
     
-    # 1. get the three largest numbers in the array
-    def compare_tuples(idx_val_tup1, idx_val_tup2):
-        return idx_val_tup1[1] > idx_val_tup2[1]
-    
-    largest = LargestList(3, compare_tuples)
+    # 1. get the largest 4 numbers in the array
+    largest = LargestList(4)
     for i in range(len(arr)):
         largest.tryAdd((i, arr[i]))
 
     # 2. find largest non-neighboring in largest list
+    
+
 
     # if first two are not neighbors- their sum is definitely largest
-    ((i1, val1), (i2, val2), (i3, val3)) = largest.list
-
-    if not are_neighbors(i1, i2):
-        return val1, val2
-    
-
-    sum13 = val1 + val3
-    sum23 = val2 + val3
-
-    if are_neighbors(i1, i3):
-        return sum23
-    elif are_neighbors(i2, i3):
-        return sum13
-    
-    return max(sum13, sum23)
     
 
 
--- daily-coding-problems/problem009_python/tests.py
+++ daily-coding-problems/problem009_python/tests.py
@@ -1,35 +1,57 @@
 import unittest
-from solution.solution import *
+from solution.solution import ListItem, LargestList, max_non_neighbor_sum
 
-class Tests(unittest.TestCase):
-    def test_example(self):
-        answer = max_non_neighbor_sum([2, 4, 6, 2, 5])
-        self.assertEqual(answer, 13)
+class LargestListTests(unittest.TestCase):
+    def test(self):
+        l = LargestList(4)
+        arr = [2, 3, 1, 4]
+        for i in range(len(arr)):
+            l.tryAdd(ListItem(i, arr[i]))
+        
+        print(f"List: {l.list}")
+        self.assertEqual(l[0].value, 4) # 4
+        self.assertEqual(l[0].idx, 3) # idx of 4
+        
+        self.assertEqual(l[1].value, 3) # 3
+        self.assertEqual(l[1].idx, 1) # idx of 3
+        
+        self.assertEqual(l[2].value, 2) # 2
+        self.assertEqual(l[2].idx, 0) # idx of 2
 
-    def test_zeros(self):
-        answer = max_non_neighbor_sum([0, 0, 0, 0])
-        self.assertEqual(answer, 0)
+        self.assertEqual(l[3].value, 1) # 1
+        self.assertEqual(l[3].idx, 2) # idx of 1
 
-    def test_simple(self):
-        answer = max_non_neighbor_sum([1, 2, 3])
-        self.assertEqual(answer, 4)
+# class SolutionTests(unittest.TestCase):
+#     def test_example(self):
+#         answer = max_non_neighbor_sum([2, 4, 6, 2, 5])
+#         self.assertEqual(answer, 13)
 
-    def test_neighboring_largest_sum(self):
-        answer = max_non_neighbor_sum([1, 3, 2])
-        self.assertEqual(answer, 3)
+#     def test_zeros(self):
+#         answer = max_non_neighbor_sum([0, 0, 0, 0])
+#         self.assertEqual(answer, 0)
 
-    def test_negative_answer(self):
-        answer = max_non_neighbor_sum([0, -1, -3, -9, -13])
-        self.assertEqual(answer, -3)
+#     def test_simple(self):
+#         answer = max_non_neighbor_sum([1, 2, 3])
+#         self.assertEqual(answer, 4)
 
-    def test_arbitrary(self):
-        answer = max_non_neighbor_sum([4, 425, 1230, 90, -1000000, 3, 15])
-        self.assertEqual(answer, 1245)
+#     def test_neighboring_largest_sum(self):
+#         answer = max_non_neighbor_sum([1, 3, 2])
+#         self.assertEqual(answer, 3)
 
-    # TODO: change name once I handle this
-    def test_unhandled_edge_case(self):
-        answer = max_non_neighbor_sum([3, 10, 5, 2])
-        self.assertEqual(12, answer)
+#     def test_negative_answer(self):
+#         answer = max_non_neighbor_sum([0, -1, -3, -9, -13])
+#         self.assertEqual(answer, -3)
+
+#     def test_arbitrary(self):
+#         answer = max_non_neighbor_sum([4, 425, 1230, 90, -1000000, 3, 15])
+#         self.assertEqual(answer, 1245)
+
+#     # TODO: change name once I handle this
+#     def test_unhandled_edge_case(self):
+#         answer = max_non_neighbor_sum([3, 10, 5, 2])
+#         self.assertEqual(12, answer)
+
+
 
 
 if __name__ == "__main__":
