595ca388fc5a3f0f2172dd9fac8abb3ff0ac1bfb
Jonathan Levin <jon.levin@inceptionxr.com>
2019-11-25 19:13:30 +0200

    Problem #010 - statement

---
+++ daily-coding-problems/problem010_python/tests.py
@@ -0,0 +1,12 @@
+import unittest
+from solution.solution import *
+
+class Tests(unittest.TestCase):
+    def test1(self):
+        pass
+
+    def test2(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
