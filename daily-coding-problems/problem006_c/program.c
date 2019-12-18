29a69ab1db405a32cdfaf23a3e0a5a88a37e88cc
Jonathan Levin <jon.levin@inceptionxr.com>
2019-12-18 17:33:23 +0200

    #006 by template

--- daily-coding-problems/problem006_c/program.c
+++ daily-coding-problems/problem006_c/program.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 
+void test();
+
+int main(){
+    test();
+    return 0;
+}
+
 // TODO: extract tests to separate file + automate compilation to task in vscode
 // TODO: learn more about c module linking/including for this
-int test(int n){
-    
+void test(){
+    printf("Hello world!\n");
 }
