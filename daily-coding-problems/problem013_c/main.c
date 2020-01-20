2f603259102695c00c12eb8cc06d1779c8ca178e
Jonathan Levin <jon.levin@inceptionxr.com>
2020-01-20 19:34:56 +0200

    WIP

--- daily-coding-problems/problem013_c/main.c
+++ daily-coding-problems/problem013_c/main.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct NodeLetterCount
+{
+    char c;
+    int count;
+} NodeLetterCount;
+
+typedef struct Node
+{
+    NodeLetterCount letterCounts[];
+
+} Node;
+
+void AddLetter(Node* n, char c) {
+    
+    NodeLetterCount letterCounts[] = n->letterCounts;
+    int l = sizeof(letterCounts) / sizeof(NodeLetterCount);
+
+    bool found = false;
+    for (int i = 0; i < l; i++) {
+        NodeLetterCount letterCount = letterCounts[i];
+        if (letterCount.c == c) {
+            letterCount.count++;
+            found = true;
+        }
+    }
+
+    if (found) return;
+
+}
 
 // TODO: extract tests to separate file + automate compilation to task in vscode
 // TODO: learn more about c module linking/including for this
